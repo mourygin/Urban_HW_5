dfe66835006a2ac9411212d8ff4c165ca5c71da3
mourygin <55830119+mourygin@users.noreply.github.com>
2024-07-26 18:15:54 +0300

    Add files via upload

--- module_8_3.py
+++ module_8_3.py
@@ -1,27 +1,27 @@
 class Car():
     def __init__(self, model, __vin, __numbers):
         self.model = model
-        self.__vin = __vin
-        if not isinstance(__vin, int):
-            raise IncorrectVinNumber('Некорректный тип vin номера')
-        elif __vin < 1000000 or __vin > 9999999:
-            raise IncorrectVinNumber('Неверный диапазон для vin номера')
-        #print(exc.message)
+        self.__vin = self.__is_valid_vin(__vin)
         self.__numbers = __numbers
         if not isinstance(__numbers, str):
             raise IncorrectCarNumbers('Некорректный тип данных для номеров')
         elif len(__numbers) != 6:
             raise IncorrectCarNumbers('Неверная длина номера')
-        #print(exc.message)
-    def __is_valid_vin(vin_number):
-        pass
-    def __is_valid_numbers(numbers):
-        pass
-
+    def __is_valid_vin(self, vin_number):
+        if not isinstance(vin_number, int):
+            raise IncorrectVinNumber('Некорректный тип vin номера')
+        elif vin_number < 1000000 or vin_number > 9999999:
+            raise IncorrectVinNumber('Неверный диапазон для vin номера')
+        return vin_number
+    def __is_valid_numbers(self, numbers):
+        if not isinstance(numbers, str):
+            raise IncorrectCarNumbers('Некорректный тип данных для номеров')
+        elif len(__numbers) != 6:
+            raise IncorrectCarNumbers('Неверная длина номера')
+        return numbers
 class IncorrectVinNumber(Exception):
     def __init__(self, message):
         self.message = message
-
 class IncorrectCarNumbers (Exception):
     def __init__(self, message):
         self.message = message
